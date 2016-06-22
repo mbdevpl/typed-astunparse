2c488bd60e1e610136d6de5590214fdbe232a1ef
Mateusz Bysiek <mb@mbdev.pl>
2016-06-22 19:02:41 +0900

    extended Unparser docs
    Change-Id: Id5d5b72ac2e1270d3c37043cdb21bdd355ede41e
    Signed-off-by: Mateusz Bysiek <mb@mbdev.pl>

--- typed_astunparse/unparser.py
+++ typed_astunparse/unparser.py
@@ -22,7 +22,168 @@ from astunparse.unparser import interleave
 import typed_ast.ast35
 
 class Unparser(astunparse.Unparser):
-    """ Partial rewrite of Unparser from astunparse to handle typed_ast.ast35-based trees. """
+    """ Partial rewrite of Unparser from astunparse to handle typed_ast.ast35-based trees.
+
+    The unparser aims at compatibility with native AST, as well as typed AST.
+
+    Quoting grammar file[1] used by CPython, below is verbatim copy of the file for Python 3.5:
+
+    "
+    -- ASDL's six builtin types are identifier, int, string, bytes, object, singleton
+    module Python
+    {
+        mod = Module(stmt* body)
+            | Interactive(stmt* body)
+            | Expression(expr body)
+
+            -- not really an actual node but useful in Jython's typesystem.
+            | Suite(stmt* body)
+
+        stmt = FunctionDef(identifier name, arguments args,
+                           stmt* body, expr* decorator_list, expr? returns)
+              | AsyncFunctionDef(identifier name, arguments args,
+                                 stmt* body, expr* decorator_list, expr? returns)
+
+              | ClassDef(identifier name,
+                 expr* bases,
+                 keyword* keywords,
+                 stmt* body,
+                 expr* decorator_list)
+              | Return(expr? value)
+
+              | Delete(expr* targets)
+              | Assign(expr* targets, expr value)
+              | AugAssign(expr target, operator op, expr value)
+
+              -- use 'orelse' because else is a keyword in target languages
+              | For(expr target, expr iter, stmt* body, stmt* orelse)
+              | AsyncFor(expr target, expr iter, stmt* body, stmt* orelse)
+              | While(expr test, stmt* body, stmt* orelse)
+              | If(expr test, stmt* body, stmt* orelse)
+              | With(withitem* items, stmt* body)
+              | AsyncWith(withitem* items, stmt* body)
+
+              | Raise(expr? exc, expr? cause)
+              | Try(stmt* body, excepthandler* handlers, stmt* orelse, stmt* finalbody)
+              | Assert(expr test, expr? msg)
+
+              | Import(alias* names)
+              | ImportFrom(identifier? module, alias* names, int? level)
+
+              | Global(identifier* names)
+              | Nonlocal(identifier* names)
+              | Expr(expr value)
+              | Pass | Break | Continue
+
+              -- XXX Jython will be different
+              -- col_offset is the byte offset in the utf8 string the parser uses
+              attributes (int lineno, int col_offset)
+
+              -- BoolOp() can use left & right?
+        expr = BoolOp(boolop op, expr* values)
+             | BinOp(expr left, operator op, expr right)
+             | UnaryOp(unaryop op, expr operand)
+             | Lambda(arguments args, expr body)
+             | IfExp(expr test, expr body, expr orelse)
+             | Dict(expr* keys, expr* values)
+             | Set(expr* elts)
+             | ListComp(expr elt, comprehension* generators)
+             | SetComp(expr elt, comprehension* generators)
+             | DictComp(expr key, expr value, comprehension* generators)
+             | GeneratorExp(expr elt, comprehension* generators)
+             -- the grammar constrains where yield expressions can occur
+             | Await(expr value)
+             | Yield(expr? value)
+             | YieldFrom(expr value)
+             -- need sequences for compare to distinguish between
+             -- x < 4 < 3 and (x < 4) < 3
+             | Compare(expr left, cmpop* ops, expr* comparators)
+             | Call(expr func, expr* args, keyword* keywords)
+             | Num(object n) -- a number as a PyObject.
+             | Str(string s) -- need to specify raw, unicode, etc?
+             | Bytes(bytes s)
+             | NameConstant(singleton value)
+             | Ellipsis
+
+             -- the following expression can appear in assignment context
+             | Attribute(expr value, identifier attr, expr_context ctx)
+             | Subscript(expr value, slice slice, expr_context ctx)
+             | Starred(expr value, expr_context ctx)
+             | Name(identifier id, expr_context ctx)
+             | List(expr* elts, expr_context ctx)
+             | Tuple(expr* elts, expr_context ctx)
+
+              -- col_offset is the byte offset in the utf8 string the parser uses
+              attributes (int lineno, int col_offset)
+
+        expr_context = Load | Store | Del | AugLoad | AugStore | Param
+
+        slice = Slice(expr? lower, expr? upper, expr? step)
+              | ExtSlice(slice* dims)
+              | Index(expr value)
+
+        boolop = And | Or
+
+        operator = Add | Sub | Mult | MatMult | Div | Mod | Pow | LShift
+                     | RShift | BitOr | BitXor | BitAnd | FloorDiv
+
+        unaryop = Invert | Not | UAdd | USub
+
+        cmpop = Eq | NotEq | Lt | LtE | Gt | GtE | Is | IsNot | In | NotIn
+
+        comprehension = (expr target, expr iter, expr* ifs)
+
+        excepthandler = ExceptHandler(expr? type, identifier? name, stmt* body)
+                        attributes (int lineno, int col_offset)
+
+        arguments = (arg* args, arg? vararg, arg* kwonlyargs, expr* kw_defaults,
+                     arg? kwarg, expr* defaults)
+
+        arg = (identifier arg, expr? annotation)
+               attributes (int lineno, int col_offset)
+
+        -- keyword arguments supplied to call (NULL identifier for **kwargs)
+        keyword = (identifier? arg, expr value)
+
+        -- import name with optional 'as' alias.
+        alias = (identifier name, identifier? asname)
+
+        withitem = (expr context_expr, expr? optional_vars)
+    }
+    "
+
+    Quoting the current docstring of "typed_ast/ast35.py"[2] from typed-ast repository:
+
+    "
+    The `ast35` module helps Python applications to process trees of the
+    Python abstract syntax grammar.  The abstract syntax itself might change
+    with each Python release; this module helps to find out programmatically
+    what the current grammar looks like and allows modifications of it.  The
+    difference between the `ast35` module and the builtin `ast` module is
+    that `ast35` provides PEP 484 type comment information as part of the
+    AST.
+
+    In particular, the `FunctionDef`, `Assign`, `For`, and `With` classes all
+    have a `type_comment` field which contains a `str` with the text of the
+    type comment.  `parse` has been augmented so it can parse function
+    signature types when called with `mode=func_type`.  Finally, `Module` has
+    a `type_ignores` field which contains a list of lines which have been
+    `# type: ignored`.
+
+    An abstract syntax tree can be generated by using the `parse()`
+    function from this module.  The result will be a tree of objects whose
+    classes all inherit from `ast35.AST`.
+
+    Additionally various helper functions are provided that make working with
+    the trees simpler.  The main intention of the helper functions and this
+    module in general is to provide an easy to use interface for libraries
+    that work tightly with the python syntax (template engines for example).
+    "
+
+    [1]: https://hg.python.org/cpython/file/tip/Parser/Python.asdl
+    [2]: https://github.com/dropbox/typed_ast/blob/master/typed_ast/ast35.py#L5
+
+    """
 
     boolops = {**astunparse.Unparser.boolops, typed_ast.ast35.And: 'and', typed_ast.ast35.Or: 'or'}
     """ Mapping from boolean operation node to its string representation.
