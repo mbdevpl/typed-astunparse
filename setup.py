af5c74f75d5a2c36945fa070007491f5ca023ee5
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-10-03 13:42:19 +0900

    runs on 3.7

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ class Package(setup_boilerplate.Package):
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Education',
